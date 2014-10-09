628acc1a69f0987dab48ee5d76abc969dbc54208
Yusra Muhtar <yusramuhtar>
2014-10-09 09:41:08 +0300

    Final

--- CurrencyConverter/src/com/yusra/project/MainActivity.java
+++ CurrencyConverter/src/com/yusra/project/MainActivity.java
@@ -31,54 +31,55 @@ public class MainActivity extends Activity {
 		final EditText dollarField = (EditText)findViewById(R.id.dollarField);
 		final EditText euroField = (EditText)findViewById(R.id.euroField);
 		final EditText goldField = (EditText)findViewById(R.id.goldField);
-		String webSite = "http://openexchangerates.org/api/latest.json?app_id=81544ed74728442fa2218b0f32d41682";
+		final String webSite = "http://openexchangerates.org/api/latest.json?app_id=81544ed74728442fa2218b0f32d41682";
 		
 		button.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				AsyncHttpClient client = new AsyncHttpClient();
+				client.get(webSite, new JsonHttpResponseHandler() {
+
+				  
+
+				    @Override
+				    public void onSuccess(int statusCode, Header[] headers, JSONObject site) {
+				        // called when response HTTP status is "200 OK"
+				    	String log = site.toString();
+				    	Log.i("lOGS", log);
+				    	try {
+							
+							JSONObject rates = site.getJSONObject("rates"); 
+							Double lira = rates.getDouble("TRY");
+							Double euro = rates.getDouble("EUR");
+							Double gold = rates.getDouble("XAU");
+							Double dollar = 1 / lira;
+							
+							Double amount= Double.valueOf(tlField.getText().toString());
+							
+							dollarField.setText(String.valueOf(dollar*amount));
+							euroField.setText(String.valueOf(euro*(dollar*amount)));
+							goldField.setText(String.valueOf(gold*(dollar*amount)));
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				   
+				    }
+
+					
+						
+					
+
+					
+				    });
 				
 			}
 			
 		});
 		
 		
-		AsyncHttpClient client = new AsyncHttpClient();
-		client.get(webSite, new JsonHttpResponseHandler() {
 
-		  
-
-		    @Override
-		    public void onSuccess(int statusCode, Header[] headers, String site) {
-		        // called when response HTTP status is "200 OK"
-		    	Log.i("lOGS", site);
-		    	try {
-					JSONObject result = new JSONObject(site);
-					JSONObject rates = result.getJSONObject("rates"); 
-					Double lira = rates.getDouble("TRY");
-					Double euro = rates.getDouble("EUR");
-					Double gold = rates.getDouble("XAU");
-					Double dollar = 1 / lira;
-					
-					Double amount= Double.valueOf(tlField.getText().toString());
-					
-					dollarField.setText(String.valueOf(dollar*amount));
-					euroField.setText(String.valueOf(euro*amount));
-					goldField.setText(String.valueOf(gold*amount));
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		   
-		    }
-
-			
-				
-			
-
-			
-		    });
 	}
 
 	
